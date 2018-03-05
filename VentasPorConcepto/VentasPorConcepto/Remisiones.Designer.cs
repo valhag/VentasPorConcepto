602b17b97757874cec76d281fd328dff93112848
valhag <valhag@terra.com.mx>
2018-03-05 13:01:53 -0600

    20180305a

---
+++ VentasPorConcepto/VentasPorConcepto/Remisiones.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace VentasPorConcepto
+{
+    partial class Remisiones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.empresasComercial1 = new Controles.EmpresasComercial();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 19);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "Fecha Final";
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 20);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "Fecha Inicial";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(108, 98);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(85, 20);
+            this.dateTimePicker2.TabIndex = 33;
+            this.dateTimePicker2.Value = new System.DateTime(2018, 3, 31, 0, 0, 0, 0);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(108, 72);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(85, 20);
+            this.dateTimePicker1.TabIndex = 32;
+            this.dateTimePicker1.Value = new System.DateTime(2018, 1, 1, 0, 0, 0, 0);
+            // 
+            // empresasComercial1
+            // 
+            this.empresasComercial1.Location = new System.Drawing.Point(12, 11);
+            this.empresasComercial1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.empresasComercial1.Name = "empresasComercial1";
+            this.empresasComercial1.Size = new System.Drawing.Size(650, 54);
+            this.empresasComercial1.TabIndex = 36;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(565, 23);
+            this.button1.TabIndex = 37;
+            this.button1.Text = "Consultar Informacion";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 172);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(681, 313);
+            this.dataGridView1.TabIndex = 38;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 492);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(565, 23);
+            this.button2.TabIndex = 39;
+            this.button2.Text = "Borrar Seleccionados";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(502, 86);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(93, 17);
+            this.checkBox1.TabIndex = 40;
+            this.checkBox1.Text = "Reporte Excel";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Remisiones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 527);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.empresasComercial1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Remisiones";
+            this.Text = "Remisiones";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Remisiones_FormClosed);
+            this.Load += new System.EventHandler(this.Remisiones_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private Controles.EmpresasComercial empresasComercial1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
---
+++ VentasPorConcepto/VentasPorConcepto/Remisiones.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using LibreriaDoctos;
+using System.IO;
+
+namespace VentasPorConcepto
+{
+    public partial class Remisiones : Form
+    {
+
+        ClassRN lrn = new ClassRN();
+        public string Cadenaconexion = "";
+        public string Archivo = "";
+        Class1 x = new Class1();
+
+        public Remisiones()
+        {
+            InitializeComponent();
+        }
+
+        private void Remisiones_Load(object sender, EventArgs e)
+        {
+
+            this.Text = " Reporte Pedido Factura " + " " + this.ProductVersion;
+            lrn.mSeteaDirectorio(Directory.GetCurrentDirectory());
+
+
+            string server = Properties.Settings.Default.server;
+            //MessageBox.Show("server " + server);
+            if (Properties.Settings.Default.server != "")
+            {
+
+                Cadenaconexion = "data source =" + Properties.Settings.Default.server +
+                ";initial catalog =" + Properties.Settings.Default.database + " ;user id = " + Properties.Settings.Default.user +
+                "; password = " + Properties.Settings.Default.password + ";";
+                //Archivo = Properties.Settings.Default.archivo;
+            }
+            if (Cadenaconexion != "")
+                empresasComercial1.Populate(Cadenaconexion);
+            else
+            {
+                Form4 x = new Form4();
+                x.Show();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime lfecha = dateTimePicker1.Value;
+            string sfecha1 = lfecha.Year.ToString() + lfecha.Month.ToString().PadLeft(2, '0') + lfecha.Day.ToString().PadLeft(2, '0');
+
+            DateTime lfecha2 = dateTimePicker2.Value;
+            string sfecha2 = lfecha2.Year.ToString() + lfecha2.Month.ToString().PadLeft(2, '0') + lfecha2.Day.ToString().PadLeft(2, '0');
+
+            // string lquery;
+
+
+
+            StringBuilder lquery = new StringBuilder();
+
+            lquery.Append("SELECT format(d.CFECHA,'dd/MM/yyyy') as FECHA, d.cfolio as FOLIO, c.CRAZONSOCIAL AS [RAZON SOCIAL],d.CTOTALUNIDADES AS [TOTAL UNIDADES], d.cneto AS NETO, d.CTOTAL AS TOTAL,co.ccodigoconcepto, d.cseriedocumento ");
+            lquery.Append("FROM admDocumentos d ");
+            lquery.Append("JOIN admClientes c on c.CIDCLIENTEPROVEEDOR = d.CIDCLIENTEPROVEEDOR ");
+            lquery.Append("JOIN admConceptos co on co.CIDCONCEPTODOCUMENTO = d.CIDCONCEPTODOCUMENTO ");
+            lquery.Append("where d.CCANCELADO = 0 and d.CIDDOCUMENTODE = 3 ");
+            lquery.Append("and d.CFECHA between '" + sfecha1 + "' and '" + sfecha2 + "' ");
+            lquery.Append("order by Fecha ");
+
+            lquery.Append("SELECT d.cfolio, p.CCODIGOPRODUCTO, p.CNOMBREPRODUCTO, m.CUNIDADES, m.CNETO, m.ctotal ");
+            lquery.Append("FROM admDocumentos d ");
+            lquery.Append("JOIN admMovimientos m on d.CIDDOCUMENTO = m.CIDDOCUMENTO ");
+            lquery.Append("join admProductos p on p.CIDPRODUCTO = m.CIDPRODUCTO ");
+            lquery.Append("where d.CCANCELADO = 0 and d.CIDDOCUMENTODE = 3 ");
+            lquery.Append("and d.CFECHA between '" + sfecha1 + "' and '" + sfecha2 + "'; ");
+            
+
+            x.mTraerInformacionComercial(lquery, empresasComercial1.aliasbdd);
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = x.DatosReporte;
+            dataGridView1.AutoResizeColumns();
+            //dataGridView1.AutoGenerateColumns = false;
+            /*DataGridViewCheckBoxColumn CBColumn = new DataGridViewCheckBoxColumn();
+            CBColumn.HeaderText = "ColumnHeader";
+            CBColumn.FalseValue = "0";
+            CBColumn.TrueValue = "1";
+            dataGridView1.Columns.Insert(0, CBColumn);*/
+
+            //dataGridView1.Columns
+
+            
+            if (checkBox1.Checked == true)
+                x.mReporteRemisionesComercial(empresasComercial1.aliasbdd, dateTimePicker1.Value, dateTimePicker2.Value);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+
+            Properties.Settings.Default.RutaEmpresaADM = empresasComercial1.aliasbdd;
+            Properties.Settings.Default.Save();
+
+            //lr.mborr
+            RegConexion newcon = new RegConexion();
+            newcon.database = empresasComercial1.aliasbdd;
+            newcon.server = Properties.Settings.Default.server;
+            newcon.usuario = Properties.Settings.Default.user;
+            newcon.ps = Properties.Settings.Default.password;
+            lrn.mAsignaEmpresaComercial(newcon);
+            foreach (DataGridViewRow x in dataGridView1.Rows)
+            {
+                
+                lrn.mBorrarDocto(x.Cells["ccodigoconcepto"].Value.ToString(), x.Cells["cseriedocumento"].Value.ToString(), x.Cells["FOLIO"].Value.ToString());
+            }
+            MessageBox.Show("Proceso Terminado");
+        }
+
+        private void Remisiones_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            lrn.mCerrarSdkComercial();
+        }
+    }
+}
